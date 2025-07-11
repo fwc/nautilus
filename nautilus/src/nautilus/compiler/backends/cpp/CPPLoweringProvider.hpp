9ae55679acfe5df6b09969186d7851b3b9791186
Matthis Gördel <matthis@posteo.de>
2025-07-11 16:06:20 +0200

    clang-format

--- nautilus/src/nautilus/compiler/backends/cpp/CPPLoweringProvider.hpp
+++ nautilus/src/nautilus/compiler/backends/cpp/CPPLoweringProvider.hpp
@@ -97,11 +97,13 @@ private:
 			blockArguments << getType(constValue->getStamp()) << " " << var << ";\n";
 			frame.setValue(constValue->getIdentifier(), var);
 
-			blocks[blockIndex] << var << " = (" << getType(constValue->getStamp()) << ")" << constValue->getValue() << ";\n";
+			blocks[blockIndex] << var << " = (" << getType(constValue->getStamp()) << ")" << constValue->getValue()
+			                   << ";\n";
 		}
 
 		template <class Type>
-		void processBinary(const std::unique_ptr<ir::Operation>& o, const std::string& operation, short blockIndex, RegisterFrame& frame) {
+		void processBinary(const std::unique_ptr<ir::Operation>& o, const std::string& operation, short blockIndex,
+		                   RegisterFrame& frame) {
 			auto op = static_cast<Type*>(o.get());
 			auto leftInput = frame.getValue(op->getLeftInput()->getIdentifier());
 			auto rightInput = frame.getValue(op->getRightInput()->getIdentifier());
