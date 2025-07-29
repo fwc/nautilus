96ac83e712222f4da6232f43a3dfa9e798bb4a5e
Matthis Gördel <matthis@posteo.de>
2025-07-29 15:38:51 +0200

    don't format yarpgen tests

--- format.sh
+++ format.sh
@@ -35,6 +35,7 @@ then
     # clang-format
     git ls-files -- '*.cpp' '*.hpp' \
       | grep --invert-match "^third_party" \
+      | grep --invert-match "^yarpgen-stuff/test_" \
       | xargs --max-args=10 --max-procs="$(nproc)" "$CLANG_FORMAT" -i
 
     # newline at eof
@@ -52,6 +53,7 @@ else
     # clang-format
     git ls-files -- '*.cpp' '*.hpp' \
       | grep --invert-match "^third_party" \
+      | grep --invert-match "^yarpgen-stuff/test_" \
       | xargs --max-args=10 --max-procs="$(nproc)" "$CLANG_FORMAT" --dry-run -Werror \
       || FAIL=1
 
