1d6c9ae7381ab81cf10e87923060a3aa106bee5e
Matthis Gördel <matthis@posteo.de>
2025-09-02 11:45:44 +0200

    generate cov_per_tests.pdf

--- summarize_cov.py
+++ summarize_cov.py
@@ -3,35 +3,77 @@
 import sys
 import json
 
+import matplotlib.pyplot as plt
 
-def main():
-    with open(sys.argv[1]) as f:
+
+def summarize_file(arg):
+    with open(arg) as f:
         gcovr_json = json.load(f)
 
-    lines_tot = 0
     lines_cov = 0
-    funct_tot = 0
+    lines_tot = 0
     funct_cov = 0
-    brnch_tot = 0
+    funct_tot = 0
     brnch_cov = 0
+    brnch_tot = 0
 
     for file in gcovr_json["files"]:
         if not file["filename"].startswith("nautilus/include") and not file["filename"].startswith("nautilus/src"):
             continue
 
-        lines_tot += file["line_total"]
         lines_cov += file["line_covered"]
-        funct_tot += file["function_total"]
+        lines_tot += file["line_total"]
         funct_cov += file["function_covered"]
-        brnch_tot += file["branch_total"]
+        funct_tot += file["function_total"]
         brnch_cov += file["branch_covered"]
+        brnch_tot += file["branch_total"]
 
-    print(f"lines: {lines_cov * 100 / lines_tot:.2f} ", end="")
-    print(f"funct: {funct_cov * 100 / funct_tot:.2f} ", end="")
-    print(f"brnch: {brnch_cov * 100 / brnch_tot:.2f}")
-
+    return lines_cov / lines_tot, funct_cov / funct_tot, brnch_cov / brnch_tot
 
 
+def main():
+
+    if sys.argv[1] != "baseline":
+        print(f"Usage: {sys.argv[0]} baseline $baseline $files...")
+        sys.exit(1)
+
+    ts_lines, ts_funct, ts_brnch = summarize_file(sys.argv[2])
+
+    covs = [summarize_file(f) for f in sys.argv[3:]]
+
+    lines, funct, brnch = zip(*covs)
+
+    # Create subplots
+    fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+
+    x_axis = range(1, len(lines) * 10, 10)
+
+    axes[0].plot(x_axis, lines, marker='o', label="YARPGen'ed tests")
+    axes[0].axhline(y=ts_lines, color='r', linestyle='--', label="handwritten tests")
+    axes[0].set_xlabel("No. of executed generated cases")
+    axes[0].set_title("Line Coverage")
+    axes[0].legend()
+
+    axes[1].plot(x_axis, funct, marker='o', label="YARPGen'ed tests")
+    axes[1].axhline(y=ts_funct, color='r', linestyle='--', label="handwritten tests")
+    axes[1].set_xlabel("No. of executed generated cases")
+    axes[1].set_title("Function Coverage")
+    axes[1].legend()
+
+    axes[2].plot(x_axis, brnch, marker='o', label="YARPGen'ed tests")
+    axes[2].axhline(y=ts_brnch, color='r', linestyle='--', label="handwritten tests")
+    axes[2].set_xlabel("No. of executed generated cases")
+    axes[2].set_title("Branch Coverage")
+    axes[2].legend()
+
+    # fig.suptitle("Comparing Data and Baseline")
+    fig.suptitle("Coverage achieved by tests generated with YARPGen", fontsize=16)
+
+    plt.tight_layout()
+
+    out_name = "cov_per_tests.pdf"
+    plt.savefig(out_name)
+    print(f"{out_name} written")
 
 
 if __name__ == "__main__":
